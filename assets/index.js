fa111f39c9f111a35ff6d4dc0f1b6782bacd7a8d
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-01-23 16:32:33 +0100

    .

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,30 @@
 let inputName = document.getElementById("inputName");
-let cognome = document.getElementById("inputCognome");
-let name = document.getElementById("inputData");
+let inputCognome = document.getElementById("inputCognome");
+let inputData = document.getElementById("inputData");
 let button = document.getElementById("button");
-let nomeCognome = document.getElementById("nomeCognome");
+let nome = document.getElementById("nome");
+let cognome = document.getElementById("cognome");
 let data = document.getElementById("data");
+
+const User = function (nome, cognome, data) {
+  this.name = nome;
+  this.cognome = cognome;
+  this.date = data;
+  this.showName = function () {
+    return this.name + " " + this.surname;
+  };
+};
+
+let currentUser = new User("Pippo", "Baudo", "12/05/1992, ");
+console.log("questo è l'utente nuovo", currentUser);
+
+button.addEventListener("click", function () {
+  nome.innerHTML = currentUser.name;
+});
+
+/* let currentUser = new User("Pippo", "Baudo", "12/05/1992");
+console.log(currentUser); */
+/* 
+button.addEventListener("click", function () {
+  nomeCognome.innerHTML = currentUser.name + currentUser.cognome;
+}); */
