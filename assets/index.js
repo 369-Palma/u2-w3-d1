915a280426bc7119da1e7edd25e69bf40e04282e
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-01-23 14:54:27 +0100

    creata struttura html e selezionati elementi in js

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+let inputName = document.getElementById("inputName");
+let cognome = document.getElementById("inputCognome");
+let name = document.getElementById("inputData");
+let button = document.getElementById("button");
+let nomeCognome = document.getElementById("nomeCognome");
+let data = document.getElementById("data");
